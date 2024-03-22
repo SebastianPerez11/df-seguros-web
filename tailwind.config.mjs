a61b079087ed3bc174672a662f4074103de0c46d
Sebastian <sebassperez11@gmail.com>
2024-03-22 14:58:22 -0300

    Push web

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			gridTemplateColumns: {
+				'auto-fit': 'repeat(auto-fit, minmax(260px, 1fr))',
+				'auto-hero': 'repeat(auto-fit, minmax(600px, 1fr))',
+				'auto-pagos': 'repeat(auto-fit, minmax(200px, 1fr))',
+				'auto-contact': 'repeat(auto-fit, minmax(400px, 1fr))',
+			}
+		},
+	},
+	plugins: [],
+}
+
