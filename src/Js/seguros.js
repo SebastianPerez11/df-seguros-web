a5f24fc56036783fa0a2d41fd227ae0c1df01e97
Sebastian <sebassperez11@gmail.com>
2024-03-24 21:02:20 -0300

    Update

---
+++ src/Js/seguros.js
@@ -0,0 +1,37 @@
+export const seguros = [
+    {
+        id: "automotores",
+        title: "Automotores",
+        icon: "../img/IconAuto.svg",
+    },
+    {
+        id: "motos",
+        title: "Motos",
+        icon: "../img/IconMoto.svg",
+    },
+    {
+        id: "bicicletas",
+        title: "Bicicletas",
+        icon: "../img/IconBicicleta.svg",
+    },
+    {
+        id: "hogar",
+        title: "Hogar",
+        icon: "../img/IconHogar.svg",
+    },
+    {
+        id: "accidentes",
+        title: "Accidentes",
+        icon: "../img/IconAccidente.svg",
+    },
+    {
+        id: "vida",
+        title: "Vida",
+        icon: "../img/IconVida.svg",
+    },
+
+
+
+
+
+]
