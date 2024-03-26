6bf7b604ea65b695ccd3d3268f31f57f3143b37e
Sebastian <sebassperez11@gmail.com>
2024-03-26 12:10:15 -0300

    Image resolving

--- src/Js/seguros.js
+++ src/Js/seguros.js
@@ -2,31 +2,31 @@ export const seguros = [
     {
         id: "automotores",
         title: "Automotores",
-        icon: "./img/IconAuto.svg",
+        icon: "../../img/IconAuto.svg",
     },
     {
         id: "motos",
         title: "Motos",
-        icon: "./img/IconMoto.svg",
+        icon: "../../img/IconMoto.svg",
     },
     {
         id: "bicicletas",
         title: "Bicicletas",
-        icon: "./img/IconBicicleta.svg",
+        icon: "../../img/IconBicicleta.svg",
     },
     {
         id: "hogar",
         title: "Hogar",
-        icon: "./img/IconHogar.svg",
+        icon: "../../img/IconHogar.svg",
     },
     {
         id: "accidentes",
         title: "Accidentes",
-        icon: "./img/IconAccidente.svg",
+        icon: "../../img/IconAccidente.svg",
     },
     {
         id: "vida",
         title: "Vida",
-        icon: "./img/IconVida.svg",
+        icon: "../../img/IconVida.svg",
     },
 ]
