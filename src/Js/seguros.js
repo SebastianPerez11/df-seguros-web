886ba0b5e5d665d7a2218f5fafb0edf20a45f995
Sebastian <sebassperez11@gmail.com>
2024-04-03 10:16:46 -0300

    Update Seo

--- src/Js/seguros.js
+++ src/Js/seguros.js
@@ -3,30 +3,36 @@ export const seguros = [
         id: "automotores",
         title: "Automotores",
         icon: "../../img/IconAuto.svg",
+        alt: "Icono Auto"
     },
     {
         id: "motos",
         title: "Motos",
         icon: "../../img/IconMoto.svg",
+        alt: "Icono Motocicleta"
     },
     {
         id: "bicicletas",
         title: "Bicicletas",
         icon: "../../img/IconBicicleta.svg",
+        alt: "Icono Bicicleta"
     },
     {
         id: "hogar",
         title: "Hogar",
         icon: "../../img/IconHogar.svg",
+        alt: "Icono Hogar"
     },
     {
         id: "accidentes",
         title: "Accidentes",
         icon: "../../img/IconAccidente.svg",
+        alt: "Icono Accidente"
     },
     {
         id: "vida",
         title: "Vida",
         icon: "../../img/IconVida.svg",
+        alt: "Icono Corazón"
     },
 ]
