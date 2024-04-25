de97bb8d9c8b5b70623a16dd0785d9dcb9b47c90
Sebastian <sebassperez11@gmail.com>
2024-04-25 11:42:23 -0300

    update

--- src/Js/seguros.js
+++ src/Js/seguros.js
@@ -1,31 +1,22 @@
 export const seguros = [
     {
-        id: "automotores",
-        title: "Automotores",
+        id: "automóviles",
+        title: "Automóviles",
         icon: "../../img/IconAuto.svg",
-        alt: "Icono Auto"
-    },
-    {
-        id: "motos",
-        title: "Motos",
-        icon: "../../img/IconMoto.svg",
-        alt: "Icono Motocicleta"
-    },
-    {
-        id: "bicicletas",
-        title: "Bicicletas",
-        icon: "../../img/IconBicicleta.svg",
-        alt: "Icono Bicicleta"
+        alt: "Icono Auto",
+        description: "Te ofrecemos protección financiera contra daños o pérdidas relacionadas con tu vehículo, al mejor precio.",
+        benefits: ["Cobertura ante robos y hurtos." , "Defensa legal y asesoramiento." , "Asistencia en ruta."],
     },
     {
         id: "hogar",
         title: "Hogar",
         icon: "../../img/IconHogar.svg",
-        alt: "Icono Hogar"
+        alt: "Icono Hogar",
+        benefits: ["Cobertura ante robos y hurtossss." , "Defensa legal y asesoramiento." , "Asistencia en ruta."],
     },
     {
-        id: "accidentes",
-        title: "Accidentes",
+        id: "combinadosFamiliar",
+        title: "Combinados Familiar",
         icon: "../../img/IconAccidente.svg",
         alt: "Icono Accidente"
     },
@@ -34,5 +25,17 @@ export const seguros = [
         title: "Vida",
         icon: "../../img/IconVida.svg",
         alt: "Icono Corazón"
+    },
+    {
+        id: "incendios",
+        title: "Incendios",
+        icon: "../../img/IconIncendio.svg",
+        alt: "Icono Incendio"
+    },
+    {
+        id: "trabajo",
+        title: "Trabajo",
+        icon: "../../img/IconTrabajo.svg",
+        alt: "Icono Trabajo"
     },
 ]
